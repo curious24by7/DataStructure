a67f82b8199dc657ad7f747af9e9bb3ae8c6f3ac
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 18:06:12 +0530

    [soham]Added: Ability to insert data between list

--- DataStructure/LinkedList.cs
+++ DataStructure/LinkedList.cs
@@ -54,5 +54,36 @@ namespace DataStructure
                 temp = temp.next;
             }
         }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while(position != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position!= 1)
+                    Console.WriteLine("Position out of range");
+            }
+            Console.WriteLine("Inserted value is :" + head);
+            return head;
+        }
     }
 }
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,9 +8,10 @@ namespace DataStructure
         {
             var linkedlist = new LinkedList();
             linkedlist.Add(56);
-            linkedlist.Add(30);
+            //linkedlist.Add(30);
             linkedlist.Add(70);
             linkedlist.Display();
+            linkedlist.InsertAtParticularPosition(2, 30);
         }
     }
 }
