f45fb8a902890f033f0f1f10ade717f1c2367b7e
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 17:41:24 +0530

    [soham]Added ability to add linked list

--- DataStructure/LinkedList.cs
+++ DataStructure/LinkedList.cs
@@ -6,5 +6,24 @@ namespace DataStructure
 {
     class LinkedList
     {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
     }
 }
