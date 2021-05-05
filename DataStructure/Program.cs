1926b3efda07b21f6adbe2557a7ebb922032681c
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 17:37:32 +0530

    [soham]Added: Linked list

---
+++ DataStructure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedlist = new LinkedList();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+        }
+    }
+}
